da25e3d945ac83ba8d49f585487a1d9e5d7d2bda
Matthew Maycock <ummaycoc@gmail.com>
2014-08-02 20:48:18 -0400

    Generates RHS of DiffEqs
    Got the right hand side of things done pretty well. Should add some
    coefficients, etc—but these are the overall forms.
    
    Given a number n as an argument, generate all the RHS’s where the
    number of y’s and the number of primes add up to that value. Pretty
    simple.

--- diffeqs.rb
+++ diffeqs.rb
@@ -23,15 +23,59 @@ class Integer
       }
     }
   end
-end
 
-class Array
-  def diffeqs
-    p self
+  def partitions
+    parts = []
+    self.partition {|p| parts << p }
+    parts
+  end
+
+  def to_y
+    if self < 1 then
+      $stderr.puts "Cannot change #{self} to_y, not big enough!"
+      exit(-1)
+    end
+
+    return 'y' + ("'" * (self - 1))
   end
 end
 
+class Array
+  def increment(sizes)
+    self[-1] += 1
+    (self.length - 1).downto(0) {|i|
+      return true if sizes[i] != self[i]
+      self[i] = 0
+      return false if i == 0
+      self[i-1] += 1
+    }
+    $stderr.puts("We should not be here!")
+    exit(-1)
+  end
 
-ARGV[0].to_i.partition {|arr|
-  arr.diffeqs
+  def enumcount
+    indices = self.map { 0 }
+    while true
+      yield(indices)
+      break unless indices.increment(self)
+    end
+  end
+
+  def diffeqs
+    splits = self.map {|i| i.partitions}
+    splits.map {|arr| arr.length}.enumcount {|indices|
+      orders = splits.zip(indices).map {|arr, i| arr[i]}
+      yield(orders.map {|term|
+        term.map {|factor| factor.to_y }
+      }.join(" + "))
+    }
+  end
+end
+
+arg = ARGV[0].to_i
+
+arg.partition {|arr|
+  arr.diffeqs {|diffeq|
+    p diffeq
+  }
 }
