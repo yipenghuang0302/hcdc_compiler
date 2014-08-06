67444f90097be432876cd581f062a895c04ec44c
ummaycoc <ummaycoc@gmail.com>
2014-08-06 18:23:07 -0400

    Starting Layout
    Starting the layout engine. Parse out all the edges now, will bin into
    integrators, multipliers, fans next.

--- layout.rb
+++ layout.rb
@@ -1,2 +1,46 @@
 #!/usr/bin/env ruby
 
+require './diffeq'
+
+class Layout
+  # ints: # of integrators
+  # muls: # of multipliers
+  # fans: # of fans
+  # mscale: mul 2 vars, scale by this
+  # fout: how much fanout we have
+  def initialize(ints=4, muls=8, fans=8, mscale=0.5, fout=3)
+    @ints = ints
+    @muls = muls
+    @fans = fans
+    @mscale = mscale
+    @fout = fout
+  end
+
+  def self.many(n)
+    Layout.new(4*n, 8*n, 8*n)
+  end
+
+  def layout(conn)
+    # Set up integrators -- this is required
+    edges, adj = [], conn[:adjlist]
+    adj.keys.sort.each {|src|
+      adj[src].keys.sort.each {|dst|
+        adj[src][dst].each {|weight|
+          edges << [ src, dst, weight ]
+        }
+      }
+    }
+
+    edges
+  end
+
+  def self.script(input, quiet=false)
+    conn = Connections.new(input, quiet)
+    p conn.instance_eval {@diffeq}
+    results = conn.connect
+    p results
+    p Layout.new.layout(results)
+  end
+end
+
+script(Layout, true) if __FILE__ == $0
