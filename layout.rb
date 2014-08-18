6e7604f898e75bcb12b01ac47684d55ab5e90d64
ummaycoc <ummaycoc@gmail.com>
2014-08-18 18:34:09 -0400

    Factor Terms
    We can now take the terms and find the various single factor and double
    factor products we need.

--- layout.rb
+++ layout.rb
@@ -1,6 +1,42 @@
 #!/usr/bin/env ruby
 
 require './diffeq'
+require 'pp'
+
+class Array
+  def delete_first(obj)
+    result = self.map {|i| i}
+    idx = result.find_index(obj)
+    result.delete_at(idx) unless idx.nil?
+    result
+  end
+
+  def factor_out(item)
+    with, without = self.partition {|term| term.include?(max)}
+    with.map! {|term| term.delete_first(max)}
+    [with, without].map {|arr| arr.factor}
+  end
+
+  def common_factor
+    counts = self.flatten.uniq..inject(Hash.new) {|h, f|
+      h.update(f => self.select {|term| term.include?(f)}.length)
+    }
+    counts.key(counts.values.max)
+  end
+  
+  def factor
+    return self if self.length == 1 && self[0].length <= 2
+    return self unless self.any? {|term| term.length > 2}
+
+    single, multiple = self.uniq.partition {|term| term.length == 1}
+    with, without = *multiple.factor_out(multiple.common_factor)
+
+    { :single => single.flatten,
+      :factor => max,
+      :across => with,
+      :other => without }
+  end
+end
 
 class Layout
   # ints: # of integrators
@@ -41,11 +77,16 @@ class Layout
         }
       }
     }
+    mults.uniq!
+
+    # These are all the y orders that come together and get multiplied
+    factors = mults.map {|src, dst, weight| dst}.factor
 
     # Now all we have to do is factor everything
     { :terms => terms,
       :mults => mults,
-      :ints => ints }
+      :ints => ints,
+      :factors => factors }
   end
 
   def self.script(input, quiet=false)
@@ -53,7 +94,7 @@ class Layout
     p conn.instance_eval {@diffeq}
     results = conn.connect
     p results
-    p Layout.new.layout(results)
+    pp Layout.new.layout(results)
   end
 end
 
