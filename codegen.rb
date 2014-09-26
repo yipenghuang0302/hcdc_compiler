0f03e1d479c491551904545cefe2f7660bd37a24
Matthew Maycock <ummaycoc@gmail.com>
2014-09-26 01:45:34 -0400

    Outputs code to file
    Can give a file to output to, which is good since a lot of debugging
    info is printed anyway.

--- codegen.rb
+++ codegen.rb
@@ -16,12 +16,26 @@ end
 end
 
 class CodeGen
-  def self.script(input, quiet=false, readout)
-    Wire.script(input, quiet, readout).each {|conn|
+  def self.script(input, quiet=false, readout=0, file=nil)
+    connections = Wire.script(input, quiet, readout)
+    begin
+      unless file.nil? then
+        error("C file must have valid extension", -1) unless file =~ /\.(c|h)(pp)?$/
+        File.open(file, File::WRONLY | File::CREAT | File::TRUNC) {|fout|
+          connections.each {|conn|
+            fout.puts conn.c_code
+            fout.puts
+          }
+        }
+      end
+    rescue Exception => e
+      error("Error working with the given file #{file}:\n\t#{e.message}", -1)
+    end
+    connections.each {|conn|
       puts conn.c_code
       puts
     }
   end
 end
 
-script(CodeGen, true, ARGV.shift.to_i) if __FILE__ == $0
+script(CodeGen, true, ARGV.shift.to_i, ARGV.shift) if __FILE__ == $0
